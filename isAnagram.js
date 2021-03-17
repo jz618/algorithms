8cbf8cdfdb6f6e6037b47bbd8a89d290b48235ab
jzhengcode <jzhengcode@gmail.com>
2021-03-16 18:19:23 -0700

    anagram

---
+++ isAnagram.js
@@ -0,0 +1,24 @@
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false
+  const counter = {};
+  
+  for (let char in s) {
+      if (!counter[char]) {
+          counter[char]++;
+      } else {
+          counter[char] = 1
+      }
+  }
+  
+  for (let char in t) {
+      if (!counter[char]) return false;
+      
+      counter[t]--;
+      if (!counter[car]) return false;
+  }
+  
+  return true
+};
+
+console.log(isAnagram("anagram",
+"nagaram"))
