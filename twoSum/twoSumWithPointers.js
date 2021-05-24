59bf1c40ea2d0dc9f4230f2c3a4a27b762977e6e
jzhengcode <jzhengcode@gmail.com>
2021-05-24 14:25:23 -0700

    twoSumWithPointers completed

--- twoSum/twoSumWithPointers.js
+++ twoSum/twoSumWithPointers.js
@@ -7,3 +7,22 @@ find a pair in the array whose sum is equal to the given target.
 Write a function to return the indices of the two numbers 
 (i.e. the pair) such that they add up to the given target.
 */
+
+function twoSumWithPointers(arr, target_sum) {
+  let leftPointer = 0;
+  let rightPointer = arr.length - 1;
+  while (leftPointer < rightPointer) {
+    let currentSum = arr[leftPointer] + arr[rightPointer];
+    if (currentSum === target_sum) return [leftPointer, rightPointer];
+
+    if (target_sum > currentSum) {
+      leftPointer++;
+    } else {
+      rightPointer--;
+    }
+  }
+
+  return [1, -1];
+}
+
+testAlgo([1, 3], twoSumWithPointers([1,2,3,4,6], 6))
