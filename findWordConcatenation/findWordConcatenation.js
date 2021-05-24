0377f72811951e623dde366dfc8847f6345f9a61
jzhengcode <jzhengcode@gmail.com>
2021-05-24 12:18:39 -0700

    added findWordConcatenation

---
+++ findWordConcatenation/findWordConcatenation.js
@@ -0,0 +1,8 @@
+import { testAlgo } from '../testAlgo.js'
+
+/*
+Given a string and a list of words
+find all the starting indices of substrings in the given string that are a concatenation of all the given words exactly once without any overlapping of words
+It is given that all words are of the same length.
+*/
+
